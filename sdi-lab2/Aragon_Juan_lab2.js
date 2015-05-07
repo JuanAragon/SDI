ad2c4aba942cb1d903f91c7169cf31d493a79c3b
Juan Aragon <juanfranaragondaza@Juans-MacBook-Pro.local>
2015-05-07 15:25:52 -0400

    DayTwoLab

---
+++ sdi-lab2/Aragon_Juan_lab2.js
@@ -0,0 +1,13 @@
+alert("JavaScript works!");
+
+var timeofWakingUp = 7;
+var beforeofTakingaShower = "put music in my Ipad";
+var goingtotheKitchen = "It\'s time to eat cereal";
+var eatingBreakfast;
+
+eatingBreakfast = confirm("Am I eating breakfast? Click OK for yes, Cancel for no.");
+
+console.log(" I wake up at " + timeofWakingUp + ".");
+console.log(" Before entering to the shower I " + beforeofTakingaShower + ".");
+console.log(" After taking a shower I know that " + goingtotheKitchen + ".");
+console.log(" It\'s " + eatingBreakfast + "I eat breakfast before going to school.");
