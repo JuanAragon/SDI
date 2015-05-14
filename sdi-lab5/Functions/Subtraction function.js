4c5234d510b3447a172ed70c97134ab35ec3039e
Juan Aragon <juanfranaragondaza@Juans-MacBook-Pro.local>
2015-05-14 16:45:43 -0400

    Subtraction Function

---
+++ sdi-lab5/Functions/Subtraction function.js
@@ -0,0 +1,27 @@
+/**
+ * Created by juanfranaragondaza on 5/14/15.
+ */
+
+//Global Variables
+var numberOne = parseFloat(prompt("Please enter a number: "));
+var numberTwo = parseFloat(prompt("Please enter a number: "));
+var typeOfOperations = prompt("Please enter the type of operation: +, -, *, / ");
+var resultOperations;
+
+function addition(){
+
+    //addition
+
+    resultOperations = (numberOne + numberTwo);
+
+    return resultOperations;
+}
+
+function subtraction(){
+
+    //subtraction
+
+    resultOperations = (numberOne - numberTwo);
+
+    return resultOperations;
+}
