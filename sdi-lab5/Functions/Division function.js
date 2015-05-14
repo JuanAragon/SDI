fc9a5f9233982bf743cb8dffe3792cd8cf7ebefa
Juan Aragon <juanfranaragondaza@Juans-MacBook-Pro.local>
2015-05-14 16:47:14 -0400

    Division Function

---
+++ sdi-lab5/Functions/Division function.js
@@ -0,0 +1,45 @@
+/**
+ * Created by juanfranaragondaza on 5/14/15.
+ */
+
+//Global Variables
+var numberOne = parseFloat(prompt("Please enter a number: "));
+var numberTwo = parseFloat(prompt("Please enter a number: "));
+var typeOfOperations = prompt("Please enter the type of operation: +, -, *, / ");
+var resultOperations;
+
+function addition(){
+
+    //addition
+
+    resultOperations = (numberOne + numberTwo);
+
+    return resultOperations;
+}
+
+function subtraction(){
+
+    //subtraction
+
+    resultOperations = (numberOne - numberTwo);
+
+    return resultOperations;
+}
+
+function multiplication(){
+
+    //multiplication
+
+    resultOperations = (numberOne * numberTwo);
+
+    return resultOperations;
+}
+
+function division(){
+
+    //division
+
+    resultOperations = (numberOne / numberTwo);
+
+    return resultOperations;
+}
