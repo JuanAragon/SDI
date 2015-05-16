bf20c4b409170425c820613d6de5b14de679329e
Juan Aragon <juanfranaragondaza@Juans-MacBook-Pro.local>
2015-05-16 16:12:55 -0400

    My Function commit

--- sdi-lab6/aragon_juan_lab6.js
+++ sdi-lab6/aragon_juan_lab6.js
@@ -1,29 +1,29 @@
 //alert("JavaScript works!");
 
-//function
+////function
 function myPrincipalFunction() {
 
-//local variables
-
-    var soccerTeams = ["Real Madrid", "Manchester United", "Bayen Munchen", "Ac Milan", "Oporto"];
-    var countriesTeam = [" Spain", " England", " Germany", " Italy", " Portugal"];
-
-    //loop
-    for (var i = 0; i < soccerTeams.length; i++) {
-
-        console.log(soccerTeams[i] + " is from " + countriesTeam[i]);
-
-    }
-
-    //pushes
-    soccerTeams.push("Millonarios");
-    console.log(soccerTeams[i] + " is the new team");
-
-    countriesTeam.push("Colombia");
-    console.log(countriesTeam[i] + " is the new country");
-
-}
-
-////main code
-myPrincipalFunction();
+////local variables
+//
+//    var soccerTeams = ["Real Madrid", "Manchester United", "Bayen Munchen", "Ac Milan", "Oporto"];
+//    var countriesTeam = [" Spain", " England", " Germany", " Italy", " Portugal"];
+//
+//    //loop
+//    for (var i = 0; i < soccerTeams.length; i++) {
+//
+//        console.log(soccerTeams[i] + " is from " + countriesTeam[i]);
+//
+//    }
+//
+//    //pushes
+//    soccerTeams.push("Millonarios");
+//    console.log(soccerTeams[i] + " is the new team");
+//
+//    countriesTeam.push("Colombia");
+//    console.log(countriesTeam[i] + " is the new country");
+//
+//}
+//
+//////main code
+//myPrincipalFunction();
 
