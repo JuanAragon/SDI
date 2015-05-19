337f375e0954208a8dfdd43cc3f5a0667e535c4c
Juan Aragon <juanfranaragondaza@Juans-MacBook-Pro.local>
2015-05-19 10:18:29 -0400

    First commit even array problem

---
+++ Day Seven/js/script.js
@@ -0,0 +1,71 @@
+/**
+ * Created by juanfranaragondaza on 5/19/15.
+ */
+//alert ("JavaScript Works!");
+
+//Lab Six Review
+
+//global variables
+var numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+var returnedArray = [];
+
+
+
+
+
+// function
+function evenArray(argArray) {
+
+
+    //local variables
+    var newArray = [];
+    var modNum;
+
+    console.log(argArray);
+
+}
+
+//main code
+returnedArray =evenArray(numberArray);
+
+
+
+
+
+
+
+
+
+
+
+////function
+//
+//function authorFunction(){
+//
+//    //local arrays
+//    var authors = ["Charles Dickens", "William Shakespeare", "Mark Twain", "Hunter S. Thompson"];
+//    var titles = [" Oliver Twist", " Hamlet", " Tom Sawyer", " Fear and Loathing in Las Vegas"];
+//
+//    //for loop to go trough the array items
+//    //output each element
+//    for (var i = 0; i < authors.length; i++){
+//
+//        console.log(authors [i] + " wrote" + titles [i] + ".");
+//
+//    }
+//
+//    //use an array method to alter the arrays
+//    authors.push("Dorothy Allison");
+//    titles.push("Bastard out of Carolina");
+//
+//    //do a final output of change
+//    console.log("I added " + authors[i] + " who wrote" + titles [i] + ".");
+//
+//}
+//
+////main code
+//
+//authorFunction();
+
+
+
