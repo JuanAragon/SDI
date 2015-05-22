7f198e338a9f33281526535142f59049a4394911
Juan Aragon <juanfranaragondaza@Juans-MacBook-Pro.local>
2015-05-21 20:30:21 -0400

    Second Commit

--- sdi-lab8/aragon_juan_lab8.js
+++ sdi-lab8/aragon_juan_lab8.js
@@ -1,6 +1,7 @@
 //alert("JavaScript works!");
 
-var returnedResult
+var returnedResult;
+var emailContainer;
 
 
 function checkedString (argumentString){
@@ -8,10 +9,22 @@ function checkedString (argumentString){
     var checkedDot;
     var checkedSpace;
     var checkedAt;
+    var result;
 
     checkedDot = argumentString.indexOf(".");
     checkedSpace = argumentString.indexOf(" ");
-    checkedAt = argumentString.indexOf(" @ ");
+    checkedAt = argumentString.indexOf("@");
 
+    if( (checkedDot >= 0) && ((checkedSpace == -1) && (checkedAt >= 0))){
 
+        result = "This is a correct email";
+    }else{
+
+        result = "This is not an email";
+    }
+
+return result;
 }
+emailContainer = prompt(" Type your email", "variable@hotmail.com");
+returnedResult = checkedString(emailContainer);
+console.log(returnedResult);
