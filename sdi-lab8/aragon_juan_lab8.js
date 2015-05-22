6fe7dfae4a0a879ee198ad1cfed6be3bd787df2e
Juan Aragon <juanfranaragondaza@Juans-MacBook-Pro.local>
2015-05-21 21:15:10 -0400

    First Commit Second Problem

--- sdi-lab8/aragon_juan_lab8.js
+++ sdi-lab8/aragon_juan_lab8.js
@@ -1,37 +1,57 @@
 //alert("JavaScript works!");
 
-//Problem 1
+////Problem 1
+//
+//var returnedResult;
+//var emailContainer;
+//
+//
+//function checkedString (argumentString){
+//
+//    var checkedDot;
+//    var checkedSpace;
+//    var checkedAt;
+//    var result;
+//    var checkedMoreAts;
+//
+//    //Methods
+//    checkedDot = argumentString.lastIndexOf(".");
+//    checkedSpace = argumentString.indexOf(" ");
+//    checkedAt = argumentString.indexOf("@");
+//    checkedMoreAts = argumentString.lastIndexOf("@");
+//
+//    if( (checkedDot >= 0) && (checkedSpace == -1) && (checkedAt >= 0) && (checkedMoreAts == checkedAt) && (checkedDot > checkedMoreAts)){
+//
+//        result = "This is a correct email";
+//    }else{
+//
+//        result = "This is not an email";
+//    }
+//
+//return result;
+//
+//}
+////Main code
+//emailContainer = prompt(" Type your email", "variable@hotmail.com");
+//returnedResult = checkedString(emailContainer);
+//console.log(returnedResult);
 
-var returnedResult;
-var emailContainer;
 
+//Problem 2
+var firstString = "cellphone-keys-wallet-earphones ";
+var secondString = "-";
+var thirdString = ",";
+var returnedValue;
 
-function checkedString (argumentString){
+function firstValue(stringArgument, separatedArgument, newSeparatedArgument){
 
-    var checkedDot;
-    var checkedSpace;
-    var checkedAt;
-    var result;
-    var checkedMoreAts;
+    var newString;
+    var newVariable;
 
-    //Methods
-    checkedDot = argumentString.lastIndexOf(".");
-    checkedSpace = argumentString.indexOf(" ");
-    checkedAt = argumentString.indexOf("@");
-    checkedMoreAts = argumentString.lastIndexOf("@");
-
-    if( (checkedDot >= 0) && (checkedSpace == -1) && (checkedAt >= 0) && (checkedMoreAts == checkedAt) && (checkedDot > checkedMoreAts)){
-
-        result = "This is a correct email";
-    }else{
-
-        result = "This is not an email";
-    }
-
-return result;
+    
 
 }
-//Main code
-emailContainer = prompt(" Type your email", "variable@hotmail.com");
-returnedResult = checkedString(emailContainer);
-console.log(returnedResult);
+
+
+
+
