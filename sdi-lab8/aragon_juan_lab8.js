51d44e252e00249e0860ee89aa096fe0b3dec3b8
Juan Aragon <juanfranaragondaza@Juans-MacBook-Pro.local>
2015-05-21 20:09:31 -0400

    First Commit

---
+++ sdi-lab8/aragon_juan_lab8.js
@@ -0,0 +1,17 @@
+//alert("JavaScript works!");
+
+var returnedResult
+
+
+function checkedString (argumentString){
+
+    var checkedDot;
+    var checkedSpace;
+    var checkedAt;
+
+    checkedDot = argumentString.indexOf(".");
+    checkedSpace = argumentString.indexOf(" ");
+    checkedAt = argumentString.indexOf(" @ ");
+
+
+}
