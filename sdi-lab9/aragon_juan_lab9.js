47bf5ad60fa3f327074fa5c161d8126f158281aa
Juan Aragon <juanfranaragondaza@Juans-MacBook-Pro.local>
2015-05-23 15:09:38 -0400

    Third Commit

--- sdi-lab9/aragon_juan_lab9.js
+++ sdi-lab9/aragon_juan_lab9.js
@@ -1,10 +1,19 @@
 //alert("JavaScript works!");
 
-//Problem 1
-var firstNumber = 5.1889664497874784477478
-var secondNumber = 3
+// Global Variables
+// Problem 1 variables
+var firstNumber = 5.1889664497874784477478;
+var secondNumber = 3;
 var returnValue;
+//Problem 2 variables
+var firstVariable = "178273678262";
+var returnResult;
+//Problem 3 variables
+var firstDate = "03/20/2015";
+var secondDate = "03/20/2016";
 
+
+// function problem 1
 function firstValue(num1, num2){
 
     num1 = num1.toFixed(num2);
@@ -13,8 +22,44 @@ function firstValue(num1, num2){
 
 }
 
+// function problem 2
+function convertToNumber(stringArg){
+
+    var result
+
+    if(isNaN(stringArg)){
+
+        result = console.log("This is not a number");
+
+    } else {
+
+        result = parseFloat(stringArg)
+
+        return result;
+    }
+}
+
+// function problem 3
+
+function convertDate(dateOne, dateTwo){
+
+    
+}
+
+
+
+
+
+//Main Code
+
+//Problem 1
 returnValue = firstValue(firstNumber, secondNumber);
-
 console.log(returnValue);
-
 console.log("The number " + firstNumber + " with 3 specified numbers is " + returnValue);
+
+//Problem 2
+returnResult = convertToNumber(firstVariable);
+console.log(returnResult);
+console.log("The string was converted to the number " + returnResult);
+
+
