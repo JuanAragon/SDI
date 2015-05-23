b1ebf1f5ca9c81277f94b8cbd900c9085772ed63
Juan Aragon <juanfranaragondaza@Juans-MacBook-Pro.local>
2015-05-23 13:57:11 -0400

    First Commit

---
+++ sdi-lab9/aragon_juan_lab9.js
@@ -0,0 +1,14 @@
+//alert("JavaScript works!");
+
+//Problem 1
+var firstNumber = 5.1889664497874784477478
+var secondnumber = 3
+var returnValue;
+
+function FirstValue(num1, num2){
+
+    num1 = num1.toFixed(num2);
+
+    return num1
+
+}
