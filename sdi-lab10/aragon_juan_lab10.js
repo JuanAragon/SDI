48aeaa46e44776f67399088b1d1a492c15a29ec2
Juan Aragon <juanfranaragondaza@Juans-MacBook-Pro.local>
2015-05-26 17:45:16 -0400

    Second Commit

--- sdi-lab10/aragon_juan_lab10.js
+++ sdi-lab10/aragon_juan_lab10.js
@@ -5,4 +5,19 @@ var returnedAnswer;
 var resultNumber;
 var numberPrompt = prompt("Please choose between this two options : Powerball or Florida Lottery");
 
+function LotteryOptions(argNumber){
 
+    var returnAnswer = [];
+
+    //If for Powerball
+    if (argNumber === "PowerBall"){
+
+        for(var i = 0; i < 5; i++) {
+
+            returnAnswer.push(Math.round(Math.random() * (59 - 1) + 1));
+        }
+
+        returnAnswer.push(Math.round(Math.random() * (35-1) + 1));
+
+    }
+}
