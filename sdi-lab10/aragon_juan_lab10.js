fefc97d6cdb8b112a35f1e0f2dbdccece9f9acc8
Juan Aragon <juanfranaragondaza@Juans-MacBook-Pro.local>
2015-05-26 17:34:20 -0400

    First Commit

---
+++ sdi-lab10/aragon_juan_lab10.js
@@ -0,0 +1,8 @@
+//alert("JavaScript works!");
+
+//Global Variables
+var returnedAnswer;
+var resultNumber;
+var numberPrompt = prompt("Please choose between this two options : Powerball or Florida Lottery");
+
+
